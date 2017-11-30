74d1ca3a416b1b7b8a8dbb4aef4df2521e4eedd0
(s) Simon Harrington <sharrington1@pm7-0089.uopnet.plymouth.ac.uk>
2017-11-30 13:54:30 +0000

    second commit

--- Canvas.js
+++ Canvas.js
@@ -1,9 +1,32 @@
 function setup(){
-   var canvas= createCanvas (1000, 1000)
+   var canvas= createCanvas (800, 800)
    canvas.parent("myContainer");
 }
 function draw(){
-  background(171,171,171)
   var text = createDiv('This is an HTML string!');
     text.position(50,50);
+    if(mouseIsPressed)
+    background(r,g,b)
+    frameRate(30)
+    {
+{
+      r = random(255);
+      g = random(255);
+      b = random(255);
+      l = random(100);
+      w = random(100);
 }
+{
+fill(r, g, b);
+
+rect(mouseX,mouseY, l, w);
+ellipse(mouseY,mouseX, l, w);
+
+  }
+{
+  let r = 255;
+  let g = 255;
+  let b = 255;
+}
+}
+    }
