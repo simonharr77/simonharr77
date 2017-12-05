85ff7aa949d6c12e1e917cbf10717d467b9e07df
(s) Simon Harrington <sharrington1@pm7-0094.uopnet.plymouth.ac.uk>
2017-12-05 15:11:10 +0000

    index and canvas

--- Canvas.js
+++ Canvas.js
@@ -1,32 +1,70 @@
+let x;
+let y;
+let speedX;
+let speedY;
+
+
 function setup(){
-   var canvas= createCanvas (800, 800)
-   canvas.parent("myContainer");
+  var canvas= createCanvas (800, 800)
+  canvas.parent("myContainer");
+  x = width/2;
+  y = height/2;
+
+  speedX = random(-10., 30);
+  speedY = random(-10., 30.);
+  r = random (255);
+  g = random (255);
+  b = random (255);
+
+  textSize(16);
+  rectMode(CENTER);
+  background(r,g,b);
+
 }
+
 function draw(){
-  var text = createDiv('This is an HTML string!');
-    text.position(50,50);
-    if(mouseIsPressed)
-    background(r,g,b)
-    frameRate(30)
-    {
-{
-      r = random(255);
-      g = random(255);
-      b = random(255);
-      l = random(100);
-      w = random(100);
-}
-{
-fill(r, g, b);
 
-rect(mouseX,mouseY, l, w);
-ellipse(mouseY,mouseX, l, w);
+  l = random(30);
+  w = random(30);
+  p = random(y,x);
+  s = random(w/8,l/8);
+  x = x + speedX;
+  y = y + speedY;
+  strokeWeight(s);
 
+
+  if (x>801){
+    fill(mouseX,mouseY,r);
+    x = p;
+    speedX = random(-5., 15.);
+    speedY = random(-5., 15.);
   }
-{
-  let r = 255;
-  let g = 255;
-  let b = 255;
+
+  if (x<-1){
+    fill(mouseX,mouseY,r);
+    x = p;
+    speedX = random(-5., 15.);
+    speedY = random(-5., 15.);
+  }
+
+  if (y>801){
+    fill(mouseX,mouseY,r);
+    y = p;
+    speedX = random(-5., 15.);
+    speedY = random(-5., 15.);
+  }
+  if (y<-1){
+    fill(mouseX,mouseY,255);
+    y = p;
+    speedX = random(-5., 15.);
+    speedY = random(-5., 15.);
+  }
+
+  fill(mouseX,mouseY,r);
+  rect(x, y, l, w);
+  ellipse(y, x, l, w);
+  frameRate(28);
+
+
+
 }
-}
-    }
